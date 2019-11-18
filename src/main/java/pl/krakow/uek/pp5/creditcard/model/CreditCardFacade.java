64e2ec527b0af1bb8bcec6da3183f0c2d33547f2
lferenc <47746737+lferenc@users.noreply.github.com>
2019-11-18 19:23:41 +0100

    update

--- src/main/java/pl/krakow/uek/pp5/creditcard/model/CreditCardFacade.java
+++ src/main/java/pl/krakow/uek/pp5/creditcard/model/CreditCardFacade.java
@@ -1,7 +1,19 @@
-package pl.krakow.uek.pp5.creditcard.model;
+package pl.uek.krakow.pp5.creditcard;
 
 public class CreditCardFacade {
+
+    private final CreditCardStorage storage;
+
+    public CreditCardFacade(CreditCardStorage creditCardStorage) {
+        this.storage = creditCardStorage;
+    }
+
     public void withdraw(WithdrawCommand withdrawCommand) {
 
+        CreditCard loaded = storage.load(withdrawCommand.getCCNumber());
+
+        loaded.withdraw(withdrawCommand.getAmount());
+
+        storage.add(loaded);
     }
 }
